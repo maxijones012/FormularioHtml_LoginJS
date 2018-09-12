acf5cdcac3f3f7d199b9889eb61b4d8ae8fc02f2
IronShield <maxijones012@gmail.com>
2018-09-11 22:38:19 -0300

    nuevos arreglos

--- main.js
+++ main.js
@@ -6,73 +6,48 @@ var valor = document.getElementById("texto").value;
 console.log(valor);*/
 
 
-function verificar() {
-
-
-
-    var textUsuario = String(document.getElementById('texttextUsuario').value);
-
-
-
-    //CREO EL NUEVO ELEMENTO
-    var nuevoElemento = document.createElement("h2"); //crea un elemento HTML especificado por su tagName
-
-
-    //--------------------------------------------------------
-    //var exp1 = new RegExp("alumno");
-    //var exp1 = /([<!]?|[<]?meta|script|)/;
-    var exp1 = /(meta|script|alert[(]?|<[!?]|<h[1-5]>)/;
-    /*
-
-                    alert[(]?       alert(
-                    <[!?]           <!,  <?
-                    <h[1-5]>        <h1>, h2, h3, h4 ,h5
-
-    
-    
-    */
-    var rta = exp1.test(textUsuario); //evalua la expresion con una cadena que ingresaste
-
-    if (rta != true) {
-        alert("verdadero: su pagina es segura: " + textUsuario);
-
-    } else {
-        alert("ERROR: SU PAGINA CONTIENE SCRIPT: " + textUsuario);
-        //res.innerHTML = "SU PAGINA ES SEGURA";
-
-
-    }
-
-}
 
 
 function verificarSQL() {
     var txtUser = document.getElementById("textUsuario");
-    var txtPass = document.getElementById("txtPass");
-    var txtRespuesta = document.getElementById("txtResultado");
+    var txtPass = document.getElementById("txtPass").value;
+    var txtResultado = document.getElementById("txtResultado");
 
-    //var expresion = /'(\s)*[;]*(AND|OR|SELECT|INTO|DROP|TABLE|COUNT)/;
-    var expresion = /'?/;
-
-
-    var result = expresion.test(txtPass);
+    //var expresion = /'(\s)*[;]*(AND|OR|SELECT|INTO|DROP|TABLE|COUNT)/
 
 
+    // var expresion = /^[A-Z]{4}\S[0-9]+$/; //si se cumple si devuelve VERDADERO
+    //var iny1 = /\s/;
+
+    var iny5 = /INSERT\s+INTO/;
+    var iny6 = /(DROP\s+TABLE)/;
+    var iny3 = /SELECT\s+COUNT([*])/;
+
+    /*
+     */
+    // var expresion = /(SELECT|INSERT|(DROP\s+TABLE)|((AND|OR)\s=))/;
+    //   var expresion = /( [']&((INSERT\s+INTO) | (DROP\s+TABLE) | (SELECT\s+COUNT([*])\s FROM)) )/;
+
+    var res1 = iny5.test(txtPass);
+    var res2 = iny3.test(txtPass);
+    var res3 = iny6.test(txtPass);
 
 
 
-    if (!result) {
+
+    if (!res1) { //ESTO NO SE CAMBIA
         alert("USUARIO Y CONTRASEÑA CORRECTOS");
-
-        var newElemt = document.createElement("h2");
-        var newContent = document.createTextNode("USUARIO Y CONTRASEÑA CORRECTOS" + "\n" + "User: " + txtUser.value + "\n" + "Pass: " + txtPass.value);
-        newElemt.appendChild(newContent);
-        document.getElementById("txtResultado").appendChild(newElemt);
-
-
+        /*
+                var newElemt = document.createElement("h2");
+                var newContent = document.createTextNode("USUARIO Y CONTRASEÑA CORRECTOS" + "\n" + "User: " + txtUser.value + "\n" + "Pass: " + txtPass.value);
+                newElemt.appendChild(newContent);
+                document.getElementById("txtResultado").appendChild(newElemt);
+                var c = document.getElementById("txtResultado");
+                c.innerHTML("hola");
+        */
 
     } else {
         alert("ERROR EN LA CONSULTA");
     }
-
+    // document.write(resultado);
 }
